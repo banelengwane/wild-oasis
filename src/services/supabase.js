96733aa46b8deab6791f5b6b6c4a0ce1887b5ba9
banelengwane <banelengwane@gmail.com>
2024-10-14 19:14:43 +0200

    Uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://ieqhgkftbgmwpaclhmwn.supabase.co'
+export const supabaseUrl = 'https://ieqhgkftbgmwpaclhmwn.supabase.co'
 // row level security will protect the use of this key when activated
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcWhna2Z0Ymdtd3BhY2xobXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0ODE5NTIsImV4cCI6MjA0NDA1Nzk1Mn0.Tu2InmUDmmmmJtM8wjl9xjPm6jsYl7iEusUTBxaWrqI'
 const supabase = createClient(supabaseUrl, supabaseKey)
