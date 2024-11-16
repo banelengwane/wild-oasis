a2be349f442779c25c0f8f937f94b5a58017b149
banelengwane <banelengwane@gmail.com>
2024-11-16 10:18:14 +0200

    detect a click outside the modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,4 +1,4 @@
-import { cloneElement, createContext, useContext, useState } from "react";
+import { cloneElement, createContext, useContext, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -71,12 +71,24 @@ function Open({children, opens: opensWindowName}){
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
-  
-  if(name !== openName) return null
+  const ref = useRef();
+  useEffect(
+    function(){
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) close();
+      }
+
+      document.addEventListener("click", handleClick, true);
+
+      return () => document.removeEventListener("click", handleClick, true)
+    },
+    [close]
+  )
+  if(name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
