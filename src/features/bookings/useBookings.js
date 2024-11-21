f07c3d1e113ef463f041112c400d18f7697997d9
banelengwane <banelengwane@gmail.com>
2024-11-21 12:12:39 +0200

    API-side sorting: Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -10,16 +10,22 @@ export function useBookings() {
   const filter =
     !filterValue || filterValue === "all"
       ? null
-      : { field: 'status', value: filterValue}
-    //   : { field: "totalPrice", value: 5000, method: 'gte' };
+      : { field: "status", value: filterValue };
+  //   : { field: "totalPrice", value: 5000, method: 'gte' };
+  
+  //Sort
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc'
+  const [field, direction] = sortByRaw.split("-")
+  const sortBy = { field, direction }
+
 
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { bookings, error, isLoading };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -9,8 +9,11 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   //filter magic
-  if (filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value);
+  if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
 
+  // sort magic
+  if (sortBy) query = query.order(sortBy.field, {ascending: sortBy.direction === 'asc'})
+  
   const { data, error } = await query;
 
   if (error) {
