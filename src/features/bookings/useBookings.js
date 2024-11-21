3445bc551d457b958dd027125764687ae77cee4f
banelengwane <banelengwane@gmail.com>
2024-11-21 11:42:01 +0200

    API-Side filtering: filtering bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
-export function useBookings(){
-    const {
-        isLoading,
-        data: bookings,
-        error
-    } = useQuery({
-        queryKey: ['bookings'],
-        queryFn: getBookings,
-    });
+export function useBookings() {
+  const [searchParams] = useSearchParams();
 
-    return { bookings, error, isLoading}
+  //Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: 'status', value: filterValue}
+    //   : { field: "totalPrice", value: 5000, method: 'gte' };
+
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
+  });
+
+  return { bookings, error, isLoading };
 }
