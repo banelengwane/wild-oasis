952ee8d828d6491f3e0fb40df4157e73e8bf9748
banelengwane <banelengwane@gmail.com>
2024-11-22 09:01:19 +0200

    API-Side Pagination: Paginting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -12,21 +12,23 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue };
   //   : { field: "totalPrice", value: 5000, method: 'gte' };
-  
-  //Sort
-  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc'
-  const [field, direction] = sortByRaw.split("-")
-  const sortBy = { field, direction }
 
+  //Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  //pagination
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { bookings, error, isLoading };
+  return { bookings, error, isLoading, count };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,11 +1,12 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)", {count: "exact"}
     );
 
   //filter magic
@@ -13,15 +14,22 @@ export async function getBookings({ filter, sortBy }) {
 
   // sort magic
   if (sortBy) query = query.order(sortBy.field, {ascending: sortBy.direction === 'asc'})
+
+  // pagination magic
+  if(page) {
+    const from = (page -1) * PAGE_SIZE
+    const to = from + PAGE_SIZE - 1
+    query = query.range(from, to)
+  }
   
-  const { data, error } = await query;
+  const { data, error, count } = await query;
 
   if (error) {
     console.log(error);
     throw new Error("Bookings could not be loaded");
   }
 
-  return data;
+  return {data, count};
 }
 
 export async function getBooking(id) {
